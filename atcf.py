8f96be02bd9c0095011e580cd2df55367108d679
Nathaniel Greenwell <61354864+ntvmb@users.noreply.github.com>
2023-09-21 21:56:32 -0500

    Load cached data upon bringing in the ATCF module

--- atcf.py
+++ atcf.py
@@ -3,11 +3,66 @@ import calendar
 import requests
 import json
 
+# initalize variables
 url = 'https://www.nrlmry.navy.mil/tcdat/sectors/atcf_sector_file'
 url_interp = 'https://www.nrlmry.navy.mil/tcdat/sectors/interp_sector_file'
+cyclones = []
+names = []
+timestamps = []
+lats = []
+longs = []
+basins = []
+winds = []
+pressures = []
+tc_classes = []
+lats_real = []
+longs_real = []
 
 class ATCFError(Exception):
     pass
+
+def load():
+    try:
+        file = open('atcf_sector_file',mode='r')
+        for line in file:
+            storm = line.split()
+            cyclones.append(storm[0])
+            names.append(storm[1])
+            time = storm[2] + storm[3]
+            # convert the timestamp from the given data to Unix time
+            timestamp = datetime.datetime(int('20'+time[0]+time[1]),int(time[2]+time[3]),int(time[4]+time[5]),int(time[6]+time[7]))
+            utc_time = calendar.timegm(timestamp.utctimetuple())
+            timestamps.append(utc_time)
+            lats.append(storm[4])
+            longs.append(storm[5])
+            basins.append(storm[6])
+            winds.append(int(storm[7]))
+            pressures.append(int(storm[8]))
+        file.close()
+    except:
+        return
+    
+    try:
+        file = open('interp_sector_file','r')
+        storms = []
+        for line in file:
+            storms.append(line.split())
+        for tc in cyclones:
+            for storm in storms:
+                cid = storm[0][2] + storm[0][3] + storm[6]
+                if cid == tc:
+                    lats_real.append(float(storm[4]))
+                    longs_real.append(float(storm[5]))
+                    tc_classes.append(storm[7])
+                    break
+            else:
+                raise ATCFError("How did you get here?")
+        file.close()
+    except:
+        raise ATCFError("Failure to get interp data")
+
+# load cached data upon bringing in the module
+load()
 
 def get_data():
     global cyclones, names, timestamps, lats, longs, basins, winds, pressures, tc_classes, lats_real, longs_real
@@ -30,41 +85,10 @@ def get_data():
         return
     open('atcf_sector_file','wb').write(ra.content)
     open('interp_sector_file','wb').write(ri.content)
-    file = open('atcf_sector_file',mode='r')
-    for line in file:
-        storm = line.split()
-        cyclones.append(storm[0])
-        names.append(storm[1])
-        time = storm[2] + storm[3]
-        # convert the timestamp from the given data to Unix time
-        timestamp = datetime.datetime(int('20'+time[0]+time[1]),int(time[2]+time[3]),int(time[4]+time[5]),int(time[6]+time[7]))
-        utc_time = calendar.timegm(timestamp.utctimetuple())
-        timestamps.append(utc_time)
-        lats.append(storm[4])
-        longs.append(storm[5])
-        basins.append(storm[6])
-        winds.append(int(storm[7]))
-        pressures.append(int(storm[8]))
-    file.close()
-    file = open('interp_sector_file','r')
-    storms = []
-    for line in file:
-        storms.append(line.split())
-    for tc in cyclones:
-        for storm in storms:
-            cid = storm[0][2] + storm[0][3] + storm[6]
-            if cid == tc:
-                lats_real.append(float(storm[4]))
-                longs_real.append(float(storm[5]))
-                tc_classes.append(storm[7])
-                break
-        else:
-            raise ATCFError("How did you get here?")
-    file.close()
-    
+    load()
     # safeguard for some situations where the main ATCF website is down
     if len(cyclones) == 0:
-        get_data_alt()
+        get_data_alt()    
 
 def get_data_alt():
     global cyclones, names, timestamps, lats, longs, basins, winds, pressures, tc_classes, lats_real, longs_real
@@ -91,6 +115,10 @@ def get_data_alt():
     with open('atcf_sector_file','w') as f:
         for d in tc_list:
             f.write(d.get('atcf_sector_file')+"\n")
+    
+    with open('interp_sector_file','w') as f:
+        for d in tc_list:
+            f.write(d.get('interp_sector_file')+"\n")
 
     for d in tc_list:
         storm = d.get('atcf_sector_file').split()
