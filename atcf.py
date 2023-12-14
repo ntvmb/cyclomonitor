2bbb299c991cf45b3b5c9eb00b3b3634250e2abe
Nathaniel Greenwell <61354864+ntvmb@users.noreply.github.com>
2023-12-13 19:54:21 -0600

    start cleaning things up
    dry up atcf.py

--- atcf.py
+++ atcf.py
@@ -1,3 +1,5 @@
+# CycloMonitor Copyright (C) 2023 Nathaniel Greenwell
+# This program comes with ABSOLUTELY NO WARRANTY; for details see main.py
 import datetime
 import calendar
 import requests
@@ -20,45 +22,51 @@ longs_real = []
 
 class ATCFError(Exception):
     pass
+def parse_storm(line: str, *, mode="std"):
+    storm = line.split()
+    if not mode == "interp":
+        cyclones.append(storm[0])
+        names.append(storm[1])
+        time = storm[2] + storm[3]
+        # convert the timestamp from the given data to Unix time
+        timestamp = datetime.datetime(int('20'+time[0]+time[1]),int(time[2]+time[3]),int(time[4]+time[5]),int(time[6]+time[7]))
+        utc_time = calendar.timegm(timestamp.utctimetuple())
+        timestamps.append(utc_time)
+        lats.append(storm[4])
+        longs.append(storm[5])
+        basins.append(storm[6])
+        winds.append(int(storm[7]))
+        pressures.append(int(storm[8]))
+    else:
+        lats_real.append(float(storm[4]))
+        longs_real.append(float(storm[5]))
+        tc_classes.append(storm[7])
+
 
 def load():
     try:
-        file = open('atcf_sector_file',mode='r')
-        for line in file:
-            storm = line.split()
-            cyclones.append(storm[0])
-            names.append(storm[1])
-            time = storm[2] + storm[3]
-            # convert the timestamp from the given data to Unix time
-            timestamp = datetime.datetime(int('20'+time[0]+time[1]),int(time[2]+time[3]),int(time[4]+time[5]),int(time[6]+time[7]))
-            utc_time = calendar.timegm(timestamp.utctimetuple())
-            timestamps.append(utc_time)
-            lats.append(storm[4])
-            longs.append(storm[5])
-            basins.append(storm[6])
-            winds.append(int(storm[7]))
-            pressures.append(int(storm[8]))
-        file.close()
-    except:
+        with open('atcf_sector_file','r') as file:
+            for line in file:
+                parse_storm(line)
+    except Exception:
         return
     
     try:
-        file = open('interp_sector_file','r')
-        storms = []
-        for line in file:
-            storms.append(line.split())
-        for tc in cyclones:
-            for storm in storms:
-                cid = storm[0][2] + storm[0][3] + storm[6]
-                if cid == tc:
-                    lats_real.append(float(storm[4]))
-                    longs_real.append(float(storm[5]))
-                    tc_classes.append(storm[7])
-                    break
-            else:
-                raise ATCFError("How did you get here?")
-        file.close()
-    except:
+        with open('interp_sector_file','r') as file:
+            storms = []
+            for line in file:
+                storms.append(line.split())
+            for tc in cyclones:
+                for storm in storms:
+                    cid = storm[0][2] + storm[0][3] + storm[6]
+                    if cid == tc:
+                        lats_real.append(float(storm[4]))
+                        longs_real.append(float(storm[5]))
+                        tc_classes.append(storm[7])
+                        break
+                else:
+                    raise ATCFError("How did you get here?")
+    except Exception:
         raise ATCFError("Failure to get interp data")
 
 # load cached data upon bringing in the module
@@ -66,17 +74,7 @@ load()
 
 def get_data():
     global cyclones, names, timestamps, lats, longs, basins, winds, pressures, tc_classes, lats_real, longs_real
-    cyclones = []
-    names = []
-    timestamps = []
-    lats = []
-    longs = []
-    basins = []
-    winds = []
-    pressures = []
-    tc_classes = []
-    lats_real = []
-    longs_real = []
+    reset()
     try:
         ra = requests.get(url, verify=False)
         ri = requests.get(url_interp,verify=False)
@@ -92,17 +90,7 @@ def get_data():
 
 def get_data_alt():
     global cyclones, names, timestamps, lats, longs, basins, winds, pressures, tc_classes, lats_real, longs_real
-    cyclones = []
-    names = []
-    timestamps = []
-    lats = []
-    longs = []
-    basins = []
-    winds = []
-    pressures = []
-    tc_classes = []
-    lats_real = []
-    longs_real = []
+    reset()
     try:
         r = requests.get("https://api.knackwx.com/atcf/v1",verify=False)
     except:
@@ -121,23 +109,8 @@ def get_data_alt():
             f.write(d.get('interp_sector_file')+"\n")
 
     for d in tc_list:
-        storm = d.get('atcf_sector_file').split()
-        cyclones.append(storm[0])
-        names.append(storm[1])
-        time = storm[2] + storm[3]
-        # convert the timestamp from the given data to Unix time
-        timestamp = datetime.datetime(int('20'+time[0]+time[1]),int(time[2]+time[3]),int(time[4]+time[5]),int(time[6]+time[7]))
-        utc_time = calendar.timegm(timestamp.utctimetuple())
-        timestamps.append(utc_time)
-        lats.append(storm[4])
-        longs.append(storm[5])
-        basins.append(storm[6])
-        winds.append(int(storm[7]))
-        pressures.append(int(storm[8]))
-        storm = d.get('interp_sector_file').split()
-        lats_real.append(float(storm[4]))
-        longs_real.append(float(storm[5]))
-        tc_classes.append(storm[7])
+        parse_storm(d.get('atcf_sector_file'))
+        parse_storm(d.get('interp_sector_file'),mode="interp")
 
 def reset():
     global cyclones, names, timestamps, lats, longs, basins, winds, pressures, tc_classes, lats_real, longs_real
